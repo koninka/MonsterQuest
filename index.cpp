7af323d0d4dc7662a12f21a76e6e6dc4ce98b3d7
monoklid <artem94@bk.ru>
2014-02-25 19:40:14 +0400

    +

--- index.cpp
+++ index.cpp
@@ -9,41 +9,61 @@
 #include <Wt/Dbo/backend/MySQL>
 #include <Wt/WEnvironment>
 #include <Wt/Json/Object>
+#include <Wt/Json/Serializer>
+#include <Wt/Http/Client>
+#include <Wt/Http/Message>
 
 using namespace Wt;
+
+#include <fstream>
+std::ofstream out("log.log");
 
 class LoginForm : public WContainerWidget 
 {
 private:
-	WLabel* label;
-	WLineEdit* usernameTextEdit;
-	WLineEdit* passwordTextEdit;
-	WPushButton* submitButton;
+	WLabel label;
+	WLineEdit usernameTextEdit;
+	WLineEdit passwordTextEdit;
+	WPushButton submitButton;
+	Http::Client client;
 	void sendLogInRequest();
+	void receiveJSONresponse(boost::system::error_code err, const Http::Message& response);
 public:
 	LoginForm(WContainerWidget* parent = 0);
 };
 
 LoginForm::LoginForm(WContainerWidget* parent) 
-	: WContainerWidget(parent)
+	: WContainerWidget(parent), 
+	label("Login", this), 
+	usernameTextEdit(this),
+	passwordTextEdit(this),
+	submitButton("Log in", this),
+	client()
 {
-	label = new WLabel("Login", this);
-
-	usernameTextEdit = new WLineEdit(this);
-	
-	passwordTextEdit = new WLineEdit(this);
-	passwordTextEdit->setEchoMode(WLineEdit::Password);
-
-	submitButton = new WPushButton("Log in", this);
-	submitButton->clicked().connect(this, &LoginForm::sendLogInRequest);
+	passwordTextEdit.setEchoMode(WLineEdit::Password);
+	submitButton.clicked().connect(this, &LoginForm::sendLogInRequest);
+	client.done().connect(this, &LoginForm::receiveJSONresponse);
 }
 
 void LoginForm::sendLogInRequest()
 {
 	Json::Object data;
-	data["username"] = usernameTextEdit->text();
-	data["password"] = passwordTextEdit->text();
-	
+	data["action"] = "login";
+	data["username"] = usernameTextEdit.text();
+	data["password"] = passwordTextEdit.text();	
+	Http::Message msg;
+	msg.addHeader("Content-Type", "application/json");
+	msg.addHeader("Accept", "application/json");
+	msg.addBodyText(Json::serialize(data));
+	client.request(Http::Post, "http://localhost", msg);
+}
+
+void LoginForm::receiveJSONresponse(boost::system::error_code err, const Http::Message& response)
+{
+	if (!err && response.status() == 200)
+	{
+		doJavaScript("alert(" + response.body() + ");");
+	}
 }
 
 class MainApplication : public WApplication
@@ -58,12 +78,19 @@ MainApplication::MainApplication(const WEnvironment& env)
 	: WApplication(env), form(new LoginForm(root())) 
 {}
 
+
+class JsonHandlerApplication : public WApplication
+{
+public:
+	JsonHandlerApplication(const WEnvironment& env): WApplication(env) {} 
+};
+
 WApplication* createApplication(const WEnvironment& env)
 {
-    return new MainApplication(env);
+	return new MainApplication(env);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     return WRun(argc, argv, &createApplication);
 }
