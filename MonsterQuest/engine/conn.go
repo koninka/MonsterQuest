afd98c7ca743b49369a75130865444dbd393e1ec
monoklid <artem94@bk.ru>
2014-03-07 14:16:44 +0400

    base for game engine

--- MonsterQuest/engine/conn.go
+++ MonsterQuest/engine/conn.go
@@ -1,5 +1,5 @@
 
-package main
+package engine
 
 import (
 	"github.com/gorilla/websocket"
@@ -21,23 +21,26 @@ const (
 type connection struct {
 	ws *websocket.Conn
 
-	send chan []byte
+	send chan interface{}
 }
 
 func (c *connection) readPump() {
 	defer func() {
-		h.unregister <- c
+		GetInstance().unregister <- c
 		c.ws.Close()
 	}()
 	c.ws.SetReadLimit(maxMessageSize)
 	c.ws.SetReadDeadline(time.Now().Add(pongWait))
 	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
-		_, message, err := c.ws.ReadMessage()
+		var json map[string] interface{}
+		err := c.ws.ReadJSON(&json)
 		if err != nil {
 			break
 		}
-		h.broadcast <- message
+		
+		// there will be a processing player's action
+
 	}
 }
 
@@ -59,7 +62,7 @@ func (c *connection) writePump() {
 				c.write(websocket.CloseMessage, []byte{})
 				return
 			}
-			if err := c.write(websocket.TextMessage, message); err != nil {
+			if err := c.ws.WriteJSON(message); err != nil {
 				return
 			}
 		case <-ticker.C:
@@ -70,15 +73,11 @@ func (c *connection) writePump() {
 	}
 }
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func ServeWs(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
-	//if r.Header.Get("Origin") != "http://"+r.Host {
-	//	http.Error(w, "Origin not allowed", 403)
-	//	return
-	//}
 	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
 	if _, ok := err.(websocket.HandshakeError); ok {
 		http.Error(w, "Not a websocket handshake", 400)
@@ -87,8 +86,8 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	c := &connection{send: make(chan []byte, 256), ws: ws}
-	h.register <- c
+	c := &connection{send: make(chan interface{}), ws: ws}
+	GetInstance().AddConnection(c)
 	go c.writePump()
 	c.readPump()
 }
--- MonsterQuest/handlers/handlers.go
+++ MonsterQuest/handlers/handlers.go
@@ -11,7 +11,6 @@ import (
     "github.com/nu7hatch/gouuid"
     "regexp"
     "html/template"
-    //"code.google.com/p/go.net/websocket"
 )
 
 var Port = ":8080"
