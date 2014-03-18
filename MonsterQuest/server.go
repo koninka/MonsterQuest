81c2037e747cfb561805a377d449e005b5970435
Mukhin Alexey <boogerm2@mail.ru>
2014-03-19 04:19:26 +1100

    add test flag to server

--- MonsterQuest/server.go
+++ MonsterQuest/server.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-    "net/http"
-    "MonsterQuest/MonsterQuest/handlers"
     "MonsterQuest/MonsterQuest/engine"
+    "MonsterQuest/MonsterQuest/handlers"
+    "net/http"
+    "os"
 )
 
 func main() {
+    var test = len(os.Args) > 1 && os.Args[1] == "test";
     http.Handle("/require/", http.StripPrefix("/require/", http.FileServer(http.Dir("./js"))))
-    http.Handle("/style/",   http.FileServer(http.Dir("./")))
-    http.Handle("/img/",   http.FileServer(http.Dir("./")))
-    http.Handle("/game/",   http.FileServer(http.Dir("./")))
-    http.Handle("/tests/",   http.FileServer(http.Dir("./")))
-    http.Handle("/wsTest/",   http.FileServer(http.Dir("./")))
-    http.Handle("/resourses/",   http.FileServer(http.Dir("./")))
+    http.Handle("/style/", http.FileServer(http.Dir("./")))
+    http.Handle("/img/", http.FileServer(http.Dir("./")))
+    http.Handle("/game/", http.FileServer(http.Dir("./")))
+    http.Handle("/resourses/", http.FileServer(http.Dir("./")))
     http.HandleFunc("/websocket", engine.ServeWs)
     http.HandleFunc("/", handlers.MainHandler)
     http.HandleFunc("/json", handlers.JsonHandler)
+    if(test){
+        http.Handle("/tests/", http.FileServer(http.Dir("./")))
+	    http.Handle("/wsTest/", http.FileServer(http.Dir("./")))
+    }
     go engine.GameLoop()
     http.ListenAndServe(handlers.Port, nil)
 }
+
