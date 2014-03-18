3e0a219ea17675343803fa1cc0eeff06ad987858
Mukhin Alexey <boogerm2@mail.ru>
2014-03-19 01:26:09 +1100

    add background class

--- MonsterQuest/js/scene.js
+++ MonsterQuest/js/scene.js
@@ -1,4 +1,28 @@
-function Scene()
+function Background(){
+	this.map = [
+		['.', '.','.','.','.'],
+		['.', '.','.','.','.'],
+		['.', '.','.','.','.'],
+		['.', '.','.','.','.'],
+		['.', '.','.','.','.'],
+		['.', '.','.','.','.']
+	]   // map from look
+	this.cells = [] // sprites objects
+	this.dictionary = {'.':'grass', '#':'wall'};
+}
+
+Background.prototype.Draw = function(graphic){
+	//this.cells = [];
+	this.cells = [];
+	for(var i = 0; i < this.map.length; ++i){
+		this.cells.push([]);
+		for(var j = 0; j < this.map[i].length; ++j){
+			x = j * TILE_SIZE;
+            y = i * TILE_SIZE;
+            this.cells[i].push(graphic.DrawObj(null, x, y, this.dictionary[this.map[i][j]]))
+		}
+	}
+}
 {
 	this.player = null;
 	this.players = [];
