3ff3312dabaa7ca9018fa12e9cb5ca85d4ecea31
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-21 01:03:58 +1100

    Add ws module

--- web/js/utils/ws.js
+++ web/js/utils/ws.js
@@ -1,18 +1,22 @@
-function WSConnect(wsuri, onload, onopen, onclose, onmessage){
+define(function() {
+   function WSConnect(wsuri, onload, onopen, onclose, onmessage) {
+      var sock = null;
+      if(wsuri) {
+         wsuri = 'ws://'+wsuri;
+      }
+      //wsuri = wsuri || "ws://localhost:8080/websocket"; // <-- note new path
+      sock = new WebSocket(wsuri);
+      sock.onopen = onopen;
+      sock.onclose = onclose;
+      sock.onmessage = onmessage;
+      sock.sendJSON = function (/*JSON*/ msg) {
+         sock.send(JSON.stringify(msg));
+         console.log(JSON.stringify(msg));
+      };
+      return sock;
+   }
 
-    var sock = null;
-    if(wsuri){
-        wsuri = 'ws://'+wsuri;
-    }
-    //wsuri = wsuri || "ws://localhost:8080/websocket"; // <-- note new path
-
-    sock = new WebSocket(wsuri);
-    sock.onopen = onopen;
-    sock.onclose = onclose;
-    sock.onmessage = onmessage;
-    sock.sendJSON = function (/*JSON*/ msg) {
-        sock.send(JSON.stringify());
-    };
-    return sock;
-}
-
+   return {
+      getNewSocket: WSConnect
+   }
+});
