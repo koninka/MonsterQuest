4176e33a2a545c043dd45a57501810d7d062ac40
Mukhin Alexey <boogerm2@mail.ru>
2014-03-21 23:01:39 +1100

    check player draw posiotion

--- web/js/game/player.js
+++ web/js/game/player.js
@@ -17,7 +17,9 @@ define(function(){
    }
 
    Player.prototype.Draw = function(graphic) {
-      this.tile = graphic.DrawObj(null, -16, -16, 'player')    
+      this.tile = graphic.DrawObj(null, 0, 0, 'player');
+      this.tile.position.x -= this.tile.texture.width / 2;
+      this.tile.position.y -= this.tile.texture.height / 2;      
       graphic.game.debug.spriteInfo(this.tile, 32, 32);
 
    }
