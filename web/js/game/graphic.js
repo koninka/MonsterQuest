5775d42d66f9233a140c32c8a923701f8d5d6555
Mukhin Alexey <boogerm2@mail.ru>
2014-03-23 01:50:29 +1100

    add random int in range function

--- web/js/game/graphic.js
+++ web/js/game/graphic.js
@@ -1,4 +1,7 @@
 define(function() {
+   function getRandomInt (min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+   }
 
    function Graphic(scene) {
       this.WIDTH = 1000;
