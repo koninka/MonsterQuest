352deff20fad231f3dc90bbc96e76b529c40024f
monoklid <artem94@bk.ru>
2014-04-01 17:16:32 +0400

    refractor code for conn struct

--- engine/conn.go
+++ engine/conn.go
@@ -5,22 +5,10 @@ import (
     "github.com/gorilla/websocket"
     "log"
     "net/http"
-    "time"
-)
-
-const (
-    writeWait = 10 * time.Second
-
-    pongWait = 60 * time.Second
-
-    pingPeriod = (pongWait * 9) / 10
-
-    maxMessageSize = 512
 )
 
 type connection struct {
     ws *websocket.Conn
-
     send chan interface{}
 }
 
@@ -29,9 +17,6 @@ func (c *connection) readPump() {
         GetInstance().unregister <- c
         c.ws.Close()
     }()
-    //c.ws.SetReadLimit(maxMessageSize)
-    //c.ws.SetReadDeadline(time.Now().Add(pongWait))
-    //c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
     for {
         var json map[string] interface{}
         err := c.ws.ReadJSON(&json)
@@ -48,30 +33,17 @@ func (c *connection) readPump() {
 }
 
 func (c *connection) write(mt int, payload []byte) error {
-    //c.ws.SetWriteDeadline(time.Now().Add(writeWait))
     return c.ws.WriteMessage(mt, payload)
 }
 
 func (c *connection) writePump() {
-    ticker := time.NewTicker(pingPeriod)
-    defer func() {
-        ticker.Stop()
-        c.ws.Close()
-    }()
+    defer c.ws.Close()
     for {
         select {
         case message := <-c.send:
-            //if !ok {
-            //    c.write(websocket.CloseMessage, []byte{})
-            //    return
-            //}
             if err := c.ws.WriteJSON(message); err != nil {
                 return
             }
-        //case <-ticker.C:
-        //    if err := c.write(websocket.PingMessage, []byte{}); err != nil {
-        //        return
-        //    }
         }
     }
 }
