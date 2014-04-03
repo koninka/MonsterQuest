1d9030391bf69586450e35c3a6b8baa2044a44b9
Mukhin Alexey <boogerm2@mail.ru>
2014-04-03 23:56:09 +1100

    decrease tick duration

--- consts/consts.go
+++ consts/consts.go
@@ -5,7 +5,7 @@ import "time"
 const (
     SERVER_PORT = ":8080"
 	VELOCITY = 0.12
-	TICK_DURATION = 100 * time.Millisecond
+	TICK_DURATION = 50 * time.Millisecond
 	DATABASE_TICK_DURATION = 1 * time.Second
 	DEFAULT_PLAYER_POS_X = 5
 	DEFAULT_PLAYER_POS_Y = 5
