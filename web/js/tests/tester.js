f36754e2ab606bd8d8b439f2201633873eb515a8
Mukhin Alexey <boogerm2@mail.ru>
2014-04-09 22:05:48 +1100

    add reg and log in tester module and login password update func

--- web/js/tests/tester.js
+++ web/js/tests/tester.js
@@ -1,4 +1,4 @@
-define(['utils/utils'], function(utils) {
+define(['utils/utils' , 'utils/ws'], function(utils, wsock) {
    var url = null;
 
    function send(data, callback) {
@@ -13,9 +13,36 @@ define(['utils/utils'], function(utils) {
       return utils.randstring();
    }
 
+   function updateData(data) {
+      data.login = 'tester' + getRandomString();
+      data.password = getRandomString()
+   }
+
+
+   function regAndLog(data){
+      send({
+         'login'    : data.login,
+         'password' : data.password,
+         'action'   : 'register'
+      }, null)
+      send({
+         'action'   : 'login',
+         'password' : data.password,
+         'login'    : data.login
+      }, function (response){
+         data.ssid     = response['sid'];
+         data.wsuri    = response['webSocket'];
+         data.actor_id = response['id'];
+         data.ws       = wsock(data.wsuri, null, null, null, null);
+      })
+
+   }
+
    return {
-      send:            send,
-      setUrl:          setUrl,
-      getRandomString: getRandomString
+      send:             send,
+      setUrl:           setUrl,
+      getRandomString:  getRandomString,
+      registerAndLogin: regAndLog,
+      updateData:       updateData
    }
 });
