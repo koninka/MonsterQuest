c87ccdc10d93305d0cafbcdbc12a8e4c9fd1a9e2
Mukhin Alexey <boogerm2@mail.ru>
2014-04-09 22:06:28 +1100

    add basic tests for test_mode

--- web/js/tests/test_mode.js
+++ web/js/tests/test_mode.js
@@ -1,47 +1,152 @@
-define(['tester', 'utils/ws'], function(tester, wsock) {
-   var ws       = null;
-   var data     = null;
-   var ssid     = null;
-   var wsuri    = null;
-   var login    = null;
+define(['tester', 'utils/ws', 'jquery'], function(tester, wsock, JQuery) {
    var expect   = chai.expect;
-   var actor_id = null;
-   var password = null;
+   var data = {
+      ssid     : null,
+      wsuri    : null,
+      login    : null,
+      actor_id : null,
+      password : null,
+      ws       : null
+   }
+   var ws = null;
 
-   function updateData() {
-      login = 'tester' + tester.getRandomString();
-      password = tester.getRandomString()
+   
+   function Prepare(){
+      tester.updateData(data);
+      tester.registerAndLogin(data);
    }
 
-   describe('Server on test mode', function() {
-      it('should successfully register', function(done) {
-         data = {
-            'login'    : login,
-            'password' : password,
-            'action'   : 'register'
-         }
-         tester.send(data, function (data) {
-            expect(data['result']).to.equal('ok');
-            done();
-         });
-      });
-      it('should successfully login', function(done) {
-         data = {
-            'action'   : 'login',
-            'password' : password,
-            'login'    : login
-         }
-         tester.send(data, function (data) {
-            expect(data['result']).to.equal('ok');
-            ssid     = data['sid'];
-            wsuri    = data['webSoсket'];
-            actor_id = data['id'];
-            done();
-         });
-      });
-   });
+   function Test(){
+      Prepare();
+      describe('Test mode', function() {
+
+          it('should take badSid', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'startTesting') {
+                  expect(response['result']).to.equal('badSid');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "startTesting", sid: "10000000"});
+         })
+
+         it('should take badSid[not a string]', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'startTesting') {
+                  expect(response['result']).to.equal('badSid');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "startTesting", sid: 1000000});
+         })
+
+         it('should take badSid[no sid send]', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'startTesting') {
+                  expect(response['result']).to.equal('badSid');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "startTesting"});
+         })
+
+         
+         it('should activate test mode', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'startTesting') {
+                  expect(response['result']).to.equal('ok');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "startTesting", sid: data.ssid});
+         })
+
+         it('should take badAction[double activation]', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'startTesting') {
+                  expect(response['result']).to.equal('badAction');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "startTesting", sid: data.ssid});
+         })
+
+         it('should fail end test mode by badSid', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'endTesting') {
+                  expect(response['result']).to.equal('badSid');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "endTesting"});
+         })
+
+         it('should end test mode', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'endTesting') {
+                  expect(response['result']).to.equal('ok');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "endTesting", sid: data.ssid});
+         })
+
+         it('should fail end test mode[double ending]', function(done){
+            ws = data.ws;
+            ws.onmessage = function(e) {
+
+               var response = JSON.parse(e.data);
+               console.log(response);
+               if (response['action'] == 'endTesting') {
+                  expect(response['result']).to.equal('badAction');
+                  ws.onmessage = undefined;
+                  done();
+               }
+            }
+            ws.sendJSON({action: "endTesting", sid: data.ssid});
+         })
+
+        
+      }); 
+   }
+   
 
    return {
-      updateData: updateData
+      Test: Test
    }
 });
