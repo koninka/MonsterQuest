53005694c4236a8d82842e2c1e1978822f2fe426
Mark Tertishniy <mtertishniy@gmail.com>
2014-04-13 14:37:59 +1100

    Add shake method to cube package
    This method set seed for random

--- cube/cube.go
+++ cube/cube.go
@@ -5,9 +5,14 @@ import (
     "time"
 )
 
+var gen *rand.Rand = rand.New(rand.NewSource(0))
+
+func Shake() {
+    gen = rand.New(rand.NewSource(int64(time.Now().Unix())))
+}
+
 func Throw(edge_amount, throws_amount int) int {
     var result int = 0
-    rand.Seed(int64(time.Now().Unix()))
     for i := 0; i < throws_amount; i++ {
         result += rand.Intn(edge_amount) + 1
     }
