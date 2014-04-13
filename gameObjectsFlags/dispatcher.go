a1507aa44611dc89d9158be0432dc3ab5ac7e542
monoklid <artem94@bk.ru>
2014-04-13 13:57:53 +0400

    add two hate-flags

--- gameObjectsFlags/dispatcher.go
+++ gameObjectsFlags/dispatcher.go
@@ -3,12 +3,16 @@ package gameObjectsFlags
 import (
 	"MonsterQuest/gameMap"
 	"MonsterQuest/gameObjectsBase"
+	"MonsterQuest/consts"
 )
 
 var flags map[string] gameObjectsBase.Flager = make(map[string] gameObjectsBase.Flager)
 
 func InitFlags(field *gameMap.GameField) {
 	flags["CAN_MOVE"] = &MoveFlag{Flag{field}}
+	flags["CAN_BLOW"] = &BlowFlag{Flag{field}}
+	flags["HATE_TROLLS"] = &HateFlag{Flag{field}, consts.TROLL}
+	flags["HATE_ORCS"] = &HateFlag{Flag{field}, consts.ORC}
 }
 
 func GetFlag(name string) gameObjectsBase.Flager {
