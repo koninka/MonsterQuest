2d35dc3fb31f2e2f9929d4599b14d725ab212879
monoklid <artem94@bk.ru>
2014-04-13 13:54:21 +0400

    remove Init func for player

--- gameObjects/player.go
+++ gameObjects/player.go
@@ -43,17 +43,11 @@ func (p *Player) GetInfo() map[string] interface{} {
 	return map[string] interface{} {"login" : p.Login}
 }
 
-func (p *Player) Init() {
-	p.Flags = append(p.Flags, gameObjectsFlags.GetFlag("CAN_MOVE"))
-}
-
 func (p *Player) Do() {
 	p.ActiveObject.Do()
 	p.Dir = -1
 }
 
 func NewPlayer(id, dbId int64, login, sid string, x, y float64) Player {
-	p := Player{gameObjectsBase.NewActiveObject(id, x, y), login, sid, dbId}
-	p.Init()
-	return p
+	return Player{gameObjectsBase.NewActiveObject(id, x, y, getPlayerKind()), login, sid, dbId}
 }
