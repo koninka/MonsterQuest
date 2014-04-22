a18d5c1c1a92af3fd6be03dad0b9a969a143b8d8
Mukhin Alexey <boogerm2@mail.ru>
2014-04-23 00:47:31 +1100

    add hp bar class

--- web/js/game/actor_w_container.js
+++ web/js/game/actor_w_container.js
@@ -1,5 +1,27 @@
 define(['options', 'global', 'actor'] ,function(OPTIONS, GLOBAL, Actor){
 
+   function HpBar(hp, max){
+      PIXI.Graphics.call(this, 0, 0);
+      this.max = max;
+      this.beginFill(0xFF0000);
+      this.drawRect(0, 0, 100, 14);
+      var m = new PIXI.Graphics(0, 0);
+      this.addChild(m);
+      this.mask = m;
+      this.SetHp(hp);
+   }
+
+   HpBar.prototype = Object.create(PIXI.Graphics.prototype);
+   HpBar.prototype.constructor = HpBar;
+
+   HpBar.prototype.SetHp = function(hp){
+      this.hp = hp;
+      this.mask.clear();
+      this.mask.beginFill(0);
+      this.mask.drawRect(0, 0, this.hp / this.max * 100, 14);
+      this.mask = this.mask;
+   }
+
    function ActorWithContainer(id, x, y, type, health, name, initAnimation, player){
       Actor.call(this, id, x, y, type, health, name);
       if(initAnimation === undefined) initAnimation = true;
