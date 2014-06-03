66fc93fffef0e646d386215b95bd5c020f1e0029
Mukhin Alexey <boogerm2@mail.ru>
2014-06-04 00:14:49 +1100

    add interation with items

--- web/js/game/inventory_item.js
+++ web/js/game/inventory_item.js
@@ -2,6 +2,17 @@ define(['global', 'OPTIONS', 'item'], function(GLOBAL, OPTIONS, Item){
 
     function InventoryItem(item){
         Item.call(this, item);
+        var m = this;
+        this.onDoubleClick = function(){
+            if(m.equiped){
+                GLOBAL.game.sendViaWS({action: "unequip", id: m.id});
+            }else{
+                GLOBAL.game.sendViaWS({action: "equip", id: m.id, slot: m.slot});
+            }
+        }
+        this.onRightClick = function(){
+            GLOBAL.game.sendViaWS({action: "drop", id: m.id});
+        }
     }
 
     InventoryItem.prototype = Object.create( Item.prototype );
--- web/js/game/item.js
+++ web/js/game/item.js
@@ -1,8 +1,15 @@
 define(['global', 'OPTIONS'], function(GLOBAL, OPTIONS){
 
-    function Item(id, type, position){
+    function Item(item){
         this.item = item;
         this.InitSprite();
+        var m = this;
+        this.onClick = function(){
+            GLOBAL.game.sendViaWS({action: "examine", id: m.id});
+        }
+        this.onRightClick = function(){
+            GLOBAL.game.sendViaWS({action: "pickUp", id: m.id});
+        }
     }
 
     Item.prototype.constructor = Item;
@@ -14,9 +21,30 @@ define(['global', 'OPTIONS'], function(GLOBAL, OPTIONS){
         //tile.rotation = this.dir;
         //this.drawable.addChild(tile);
         this.drawable = tile;
+        tile.interactive = true;
+        var m = this;
+        tile.click = function(data){
+            var now = Date.now();
+            var lc = tile.click.lastClick;
+            var diff = now - lc;
+            var event = data.originalEvent;
+            if(event.which == 3 || event.button == 2) {
+                if(m.onClick)
+                    m.onClick();
+            } else if(lc && (diff < 350)) {
+                tile.click.lastClick = 0;
+                if(m.onDoubleClick)
+                    m.onDoubleClick();
+            } else {
+                tile.click.lastClick = now;
+                if(m.onRightClick)
+                    m.onRightClick();
+            }
+        }
+        this.container.click.lastClick = 0;
     }
 
-    Item.prototype.SetPosition = function (pos){
+    Item.prototype.Move = function (pos){
         this.item.position = pos;
         this.drawable.position.x = (pos - player.pt.x) * OPTIONS.TILE_SIZE - OPTIONS.TILE_SIZE / 2;
         this.drawable.position.y = (pos - player.pt.y) * OPTIONS.TILE_SIZE - OPTIONS.TILE_SIZE / 2;
