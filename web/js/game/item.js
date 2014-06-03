ceb658aeeab00d0fa64b22654b14a3f19a67ffbe
Mukhin Alexey <boogerm2@mail.ru>
2014-06-03 21:28:09 +1100

    refactor item

--- web/js/game/item.js
+++ web/js/game/item.js
@@ -1,20 +1,14 @@
 define(['global', 'OPTIONS'], function(GLOBAL, OPTIONS){
 
     function Item(id, type, position){
-        this.id = id;
-        this.type = type;
-        this.position = position;
+        this.item = item;
+        this.InitSprite();
     }
 
     Item.prototype.constructor = Item;
 
-    function InventoryItem(id, type, cell){
-        Item.call(this, id, type, cell);
-        this.InitSprite();
-    }
-
-    InventoryItem.prototype.InitSprite = function (){
-        var tile = GLOBAL.graphic.Sprite(this.type);
+    Item.prototype.InitSprite = function (){
+        var tile = GLOBAL.graphic.Sprite(this.item.name);
         tile.anchor.x = 0.5;
         tile.anchor.y = 0.5;
         //tile.rotation = this.dir;
@@ -22,13 +16,12 @@ define(['global', 'OPTIONS'], function(GLOBAL, OPTIONS){
         this.drawable = tile;
     }
 
-    InventoryItem.prototype.SetCell = function (cell){
-        this.position = cell;
-        this.drawable.position = {x: cell.x * OPTIONS.TILE_SIZE, y: cell.y * OPTIONS.TILE_SIZE};
+    Item.prototype.SetPosition = function (pos){
+        this.item.position = pos;
+        this.drawable.position.x = (pos - player.pt.x) * OPTIONS.TILE_SIZE - OPTIONS.TILE_SIZE / 2;
+        this.drawable.position.y = (pos - player.pt.y) * OPTIONS.TILE_SIZE - OPTIONS.TILE_SIZE / 2;
+        GLOBAL.graphic.Center(this.drawable);
     }
 
-    return {
-        Item: Item,
-        InventoryItem: InventoryItem
-    }
+    return Item;
 })
