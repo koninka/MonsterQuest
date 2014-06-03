7e009e27711a7d8614d4452a54591cba9ec40a43
monoklid <artem94@bk.ru>
2014-06-04 00:45:05 +0400

    add test item drop after mobs death

--- engine/mobList.go
+++ engine/mobList.go
@@ -7,6 +7,7 @@ import (
     "time"
     "MonsterQuest/gameFight/blows"
     "MonsterQuest/gameObjects"
+    "MonsterQuest/gameObjectsBase"
     "MonsterQuest/connect"
     "MonsterQuest/utils"
     "MonsterQuest/consts"
@@ -25,6 +26,12 @@ func (ml *mobList) takeAwayMob(m *gameObjects.Mob) {
     for _, item := range m.GetItems() {
         GetInstance().field.LinkToCells(item)
     }
+    // there is only in develop production
+    item := gameObjectsBase.NewItem(GenerateId(), m.GetCenter().X, m.GetCenter().Y,
+        gameObjectsBase.NewItemKind("test", "testItem", consts.ITEM_WEAPON))
+    GetInstance().items.addItem(item)
+    GetInstance().field.LinkToCells(item)
+    //  -----
     time.Sleep(consts.LIVING_AFTER_DEAD_DURATION)
     delete(ml.mobs, m.GetID())
     GetInstance().field.UnlinkFromCells(m)
