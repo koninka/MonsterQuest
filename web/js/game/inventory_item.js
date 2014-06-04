01421adab6ea0bc92a5d0a1ebb5ed3ae6481b771
Mukhin Alexey <boogerm2@mail.ru>
2014-06-04 23:57:31 +1100

    add draging items in inventory

--- web/js/game/inventory_item.js
+++ web/js/game/inventory_item.js
@@ -3,21 +3,56 @@ define(['global', 'OPTIONS', 'item'], function(GLOBAL, OPTIONS, Item){
         return undefined;
     }
 
+    function CoordsToCellNumb(coords){
+        var cell = {};
+        cell.x = Math.floor(coords.x / OPTIONS.TILE_SIZE);
+        cell.y = Math.floor(coords.y / OPTIONS.TILE_SIZE);
+        if(cell.x < 0 || cell.y < 0 || cell.x >= OPTIONS.inventory_size.x || cell.y >= OPTIONS.inventory_size.y)
+            return null;
+        var n = cell.x + cell.y * OPTIONS.inventory_size.x;
+        return n;
+    }
+
     function InventoryItem(item){
         Item.call(this, item);
         var m = this.item;
-        this.onDoubleClick = function(){
+        this.onDoubleClick = function(data){
+            data.originalEvent.preventDefault();
             if(m.equiped){
                 GLOBAL.game.sendViaWS({action: "unequip", id: m.id});
             }else{
                 GLOBAL.game.sendViaWS({action: "equip", id: m.id, slot: fund_slot(m)});
             }
         }
-        this.onClick = function(){
-            GLOBAL.game.sendViaWS({action: "examine", id: m.id});
+        this.onRightClick = function(data){
+            data.originalEvent.preventDefault();
+            var keys = KeyboardJS.activeKeys();
+            if(keys[0] == 'shift')
+                GLOBAL.game.sendViaWS({action: "drop", id: m.id});
+            else
+                GLOBAL.game.sendViaWS({action: "examine", id: m.id});
         }
-        this.onRightClick = function(){
-            GLOBAL.game.sendViaWS({action: "drop", id: m.id});
+        this.drawable.mousedown = function(data){
+            data.originalEvent.preventDefault();
+            this.data = data;
+            this.alpha = 0.9;
+            this.dragging = true;
+        }
+        this.drawable.mouseup = this.drawable.mouseupoutside = function(data){
+            this.alpha = 1;
+            this.dragging = false;
+            //var newPosition = this.data.getLocalPosition(this.parent);
+            this.data = null;
+            var cell = CoordsToCellNumb(this.position);
+            if(cell !== null)
+                GLOBAL.game.sendViaWS({action: "moveItem", id: m.id, cell: cell});
+        }
+        this.drawable.mousemove =  function(data){
+            if(this.dragging){
+                var newPosition = this.data.getLocalPosition(this.parent);
+                this.position.x = newPosition.x;
+                this.position.y = newPosition.y;
+            }
         }
     }
 
@@ -27,7 +62,8 @@ define(['global', 'OPTIONS', 'item'], function(GLOBAL, OPTIONS, Item){
     InventoryItem.prototype.SetPosition = function (cell){
         this.item.x = cell.x;
         this.item.y = cell.y;
-        this.drawable.position = {x: cell.x * OPTIONS.TILE_SIZE, y: cell.y * OPTIONS.TILE_SIZE};
+        if(!this.drawable.dragging)
+            this.drawable.position = {x: cell.x * OPTIONS.TILE_SIZE, y: cell.y * OPTIONS.TILE_SIZE};
     }
 
     return InventoryItem;
