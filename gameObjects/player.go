6883127f5bb368b652286bc8a3626eea011f535a
monoklid <artem94@bk.ru>
2014-06-05 16:25:13 +0400

    code refractor: change functions' order

--- gameObjects/player.go
+++ gameObjects/player.go
@@ -100,6 +100,11 @@ func (p *Player) Unequip(slotName string) bool {
     return true
 }
 
+func (p *Player) GetHit(blow fightBase.Blower, attacker gameObjectsBase.Activer) consts.JsonType {
+    res := p.ActiveObject.GetHit(blow, attacker)
+    return res
+}
+
 func (p *Player) Equipped(item *gameObjectsBase.Item) bool {
     for _, slot := range p.slots {
         if slot.item == item {
@@ -131,9 +136,4 @@ func NewPlayer(id, dbId int64, login, sid string, x, y float64) Player {
     slots["gloves"] = newSlot(consts.ITEM_T_GLOVES)
     return Player{gameObjectsBase.NewActiveObject(id, consts.INITIAL_PLAYER_HP, x, y, getPlayerKind()),
         login, sid, dbId, wpns.GetWeapon(consts.FIST_WEAP), slots}
-}
-
-func (p *Player) GetHit(blow fightBase.Blower, attacker gameObjectsBase.Activer) consts.JsonType {
-    res := p.ActiveObject.GetHit(blow, attacker)
-    return res
 }
