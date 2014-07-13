5c3a8fabf34a301aee40318f60b79503488ad79d
Mukhin Alexey <boogerm2@mail.ru>
2014-07-14 00:12:24 +1100

    add state text, confirm btn to trackbar

--- web/js/game/trackbar.js
+++ web/js/game/trackbar.js
@@ -1,22 +1,19 @@
 
 define(["global", "options"], function(GLOBAL, OPTIONS){
 
+    function Text(text){
+        return GLOBAL.graphic.Text(
+            text, 
+            {'font': '12px Helvetica', 'font-weight': 'bold', fill: 'white', wordWrap : true, wordWrapWidth : 200},
+            0, 
+            0
+        );
+    }
+
     function SliderMouseDown(data){
         data.originalEvent.preventDefault();
         this.data = data;
         this.dragging = true;
-    }
-
-    function SliderMouseMove(data){
-        if(this.dragging){
-            var x = this.data.getLocalPosition(this.parent).x;
-            if(x < 0)
-                x = 0;
-            if(x > this.parent.width)
-                x = this.parent.width;
-            this.position.x = x;
-            //this.position.y = newPosition.y;
-        }
     }
 
     function SliderMouseUp(data){
@@ -32,13 +29,15 @@ define(["global", "options"], function(GLOBAL, OPTIONS){
         this.cur = 0;
         this.low = 0;
         this.high = 0;
-        var bar = GLOBAL.graphic.Sprite('trackbar_bar');
+        var bar = this.bar = GLOBAL.graphic.Sprite('trackbar_bar');
         var background = GLOBAL.graphic.Sprite('trackbar_background');
-        var slider = GLOBAL.graphic.Sprite('trackbar_slider');
+        var slider = this.slider = GLOBAL.graphic.Sprite('trackbar_slider');
         var exit = GLOBAL.graphic.Sprite('trackbar_exit');
+        var ok = this.ok = GLOBAL.graphic.Sprite('trackbar_ok');
         this.addChild(background);
         background.addChild(bar);
         background.addChild(exit);
+        background.addChild(ok);
         bar.position = OPTIONS.trackbar.bar_position;
         bar.addChild(slider);
 
@@ -47,26 +46,32 @@ define(["global", "options"], function(GLOBAL, OPTIONS){
         slider.position.y += bar.height / 2;
         slider.interactive = true;
         slider.mousedown = SliderMouseDown;
-        slider.mousemove = SliderMouseMove;
+        slider.mousemove = function(data){
+            if(this.dragging){
+                var x = this.data.getLocalPosition(this.parent).x;
+                if(x < 0)
+                    x = 0;
+                if(x > this.parent.width)
+                    x = this.parent.width;
+                this.position.x = x;
+                I.SetCur(Math.floor(x * (I.high - I.low) / bar.width));
+            }
+        }
         slider.mouseup = slider.mouseupoutside = SliderMouseUp;
-
         exit.anchor.x = exit.anchor.y = 0.5;
-        exit.position.x = background.width - 5;
+        exit.position.x = background.width - 10;
         exit.position.y = 5;
         exit.interactive = true;
         exit.click = function(){
             I.Hide();
         }
 
-        //this.bar.onmousemove = function(){
-        //    I.cur = (high_pix - low_pix) / I.high * cur_pix;
-        //}
-        //this.button = new PIXI.Sprite('button');
-        //this.addChild(this.slider);
-        //this.addChild(this.bar);
-        //this.addChild(this.button);
-        //this.high_text;
-        //this.low_text;
+        ok.anchor.x = 0.5;
+        ok.anchor.y = 1;
+        ok.position.x = background.width / 2;
+        ok.position.y = background.height - 5;
+        ok.interactive = true;
+
     }
 
     TrackBar.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
@@ -79,12 +84,67 @@ define(["global", "options"], function(GLOBAL, OPTIONS){
     TrackBar.prototype.Hide = function(){
         this.visible = false;
     }
-    //
-    //
 
     TrackBar.prototype.SetHigh = function(high){
         this.high = high;
+        if(this.high_text)
+            this.removeChild(this.high_text);
+        var text = this.high_text = Text(high);
+        var bp = this.bar.position;
+        text.anchor.x = 1;
+        text.anchor.y = 1;
+        text.position.x = bp.x + this.bar.width;
+        text.position.y = bp.y //- 10;
+        this.addChild(this.high_text);
+    }
 
+    TrackBar.prototype.SetLow = function(low) {
+        this.low = low;
+        if(this.low_text)
+            this.removeChild(this.low_text);
+        var text = this.low_text = Text(low);
+        var bp = this.bar.position;
+        text.anchor.y = 1;
+        text.position.x = bp.x;
+        text.position.y = bp.y
+        this.addChild(this.low_text);
+    }
+
+    TrackBar.prototype.SetData = function(data){
+        var I = this;
+        this.ok.click = function(){
+            data.amount = I.cur;
+            GLOBAL.game.sendViaWS(data);
+            I.Hide();
+            GLOBAL.game.SelfExamine();
+        }
+    }
+
+    TrackBar.prototype.SetCur = function(cur){
+        this.cur = cur;
+        if(this.cur_text)
+            this.removeChild(this.cur_text);
+        var text = this.cur_text = Text(cur);
+        var bp = this.bar.position;
+        text.anchor.x = 0.5;
+        text.anchor.y = 1;
+        text.position.x = bp.x + this.bar.width / 2;
+        text.position.y = bp.y
+        this.addChild(this.cur_text);
+    }
+
+    TrackBar.prototype.SetSlider = function(cur){
+        this.slider.position.x = cur * this.bar.width / (this.high - this.low);
+    }
+
+    TrackBar.prototype.SetAndShow = function(low, high, data){
+        this.cur = 0;
+        this.SetHigh(high);
+        this.SetLow(low);
+        this.SetCur(0);
+        this.SetSlider(this.cur);
+        this.SetData(data);
+        this.Show();
     }
 
     TrackBar.prototype.Init = function(){
