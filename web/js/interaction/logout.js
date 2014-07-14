7775ffd5f101fb6cf295f85aeac11e7e9a30ac9e
Mukhin Alexey <boogerm2@mail.ru>
2014-07-14 21:37:41 +1100

    comment alerts

--- web/js/interaction/logout.js
+++ web/js/interaction/logout.js
@@ -1,9 +1,9 @@
 define(['jquery', 'utils/utils'], function(JQuery, utils) {
    function logoutSuccess(data, textStatus, jqXHR) {
       if(data['result'] == 'ok'){
-         utils.gameShutDown("You have successfully logged out");
+         //utils.gameShutDown("You have successfully logged out");
       } else if(data['result'] == 'badSid'){
-         utils.gameShutDown("You have successfully logged out");
+         //utils.gameShutDown("You have successfully logged out");
       } else {
          alert('Нет ответа от сервера');
       }
