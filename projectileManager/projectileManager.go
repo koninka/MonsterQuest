1ff841977cb9da4d1f05d04809a00fa1dd497781
monoklid <artem94@bk.ru>
2014-07-22 17:10:20 +0400

    fix no compiled code

--- projectileManager/projectileManager.go
+++ projectileManager/projectileManager.go
@@ -6,6 +6,7 @@ import (
     "MonsterQuest/geometry"
     "MonsterQuest/consts"
     "MonsterQuest/utils"
+    "MonsterQuest/gameObjectsBase"
     pM "MonsterQuest/projectileManager/projectiles"
 )
 
@@ -14,24 +15,28 @@ type ProjectileManager struct {
     projectiles map[int64] pM.Projectiler
 }
 
+func (pm *ProjectileManager) CheckCollision(p pM.Projectiler) (bool, gameObjectsBase.Activer) {
+    return false, nil
+}
+
 func (pm *ProjectileManager) Do() {
     for _, p := range pm.projectiles {
         var shift float64
-        d := geometry.Distance(p.Center, p.Destination)
+        d := geometry.Distance(p.GetCenter(), p.GetDestination())
         if d > consts.PROJECTILE_VELOCITY {
             shift = consts.PROJECTILE_VELOCITY
         } else {
             shift = d
         }
-        x := p.Destination.X - p.Center.X
-        y := p.Destination.Y - p.Center.Y
+        x := p.GetDestination().X - p.GetCenter().X
+        y := p.GetDestination().Y - p.GetCenter().Y
         norm := math.Sqrt(x * x + y * y)
         dx := x * shift / norm
         dy := y * shift / norm
         p.Shift(dx, dy)
-        if collisionOccured, actor := pm.CheckCollision(); collisionOccured {
+        if collisionOccured, actor := pm.CheckCollision(p); collisionOccured {
             if p.(*pM.AreaDamageProjectile) != nil {
-                
+
             } else {
                 if actor != nil {
                     // actor.GetHit(???)
@@ -42,11 +47,13 @@ func (pm *ProjectileManager) Do() {
 }
 
 func (pm *ProjectileManager) NewArrowProjectile(start, finish *geometry.Point, damage int) {
-    pm.projectiles[id] = pM.NewProjectile(utils.GenerateId(), start, finish, damage)
+    id := utils.GenerateId()
+    pm.projectiles[id] = pM.NewProjectile(id, start, finish, damage)
 }
 
 func (pm *ProjectileManager) NewFireBallProjectile(start, finish *geometry.Point, damage, radius int) {
-    pm.projectiles[id] = pM.NewAreaDamageProjectile(utils.GenerateId(), start, finish, damage, radius)
+    id := utils.GenerateId()
+    pm.projectiles[id] = pM.NewAreaDamageProjectile(id, start, finish, damage, radius)
 }
 
 func NewProjectileManager(field *gameMap.GameField) *ProjectileManager {
