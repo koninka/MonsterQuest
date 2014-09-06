a6e8bfaf07970b32dece21c7124e249657d69bae
Mukhin Alexey <boogerm2@mail.ru>
2014-09-07 01:08:41 +1100

    make count label as module

---
+++ web/js/game/count_label.js
@@ -0,0 +1,41 @@
+define(['global', 'options'], function(GLOBAL, OPTIONS){ 
+    function CountLabel(){
+        PIXI.DisplayObjectContainer.call(this);
+        //this.draw = undefined
+        //this.SetCount(count);
+    }
+    
+    CountLabel.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+    CountLabel.prototype.constructor = CountLabel;
+    
+    CountLabel.prototype.SetCount = function(count){
+        if (this.count == count) return;
+        this.count = count;
+        if(this.draw)
+            this.removeChild(this.draw);
+        if(this.count == undefined)
+            this.count = 1;
+        this.draw = GLOBAL.graphic.Text(
+            this.count, 
+            {'font': '12px Helvetica', 'font-weight': 'bold', fill: 'white', wordWrap : true, wordWrapWidth : 200},
+            0, 
+            0
+        );
+        this.draw.position = {x : 0, y : 0}
+        this.addChild(this.draw);
+        if(this.count < 2)
+            this.Hide()
+        else
+            this.Show();
+    }
+    
+    CountLabel.prototype.Show = function(){
+        this.visible = true;
+    }
+    
+    CountLabel.prototype.Hide = function(){
+        this.visible = false;
+    }
+
+    return CountLabel;
+});
--- web/js/game/inventory_item.js
+++ web/js/game/inventory_item.js
@@ -1,41 +1,4 @@
-define(['global', 'options', 'item', 'trackbar'], function(GLOBAL, OPTIONS, Item, trackbar){
-    
-    function CountLabel(count){
-        PIXI.DisplayObjectContainer.call(this);
-        //this.draw = undefined
-        //this.SetCount(count);
-    }
-    
-    CountLabel.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
-    CountLabel.prototype.constructor = CountLabel;
-    
-    CountLabel.prototype.SetCount = function(count){
-        this.count = count;
-        if(this.draw)
-            this.removeChild(this.draw);
-        if(this.count == undefined)
-            this.count = 1;
-        this.draw = GLOBAL.graphic.Text(
-            this.count, 
-            {'font': '12px Helvetica', 'font-weight': 'bold', fill: 'white', wordWrap : true, wordWrapWidth : 200},
-            0, 
-            0
-        );
-        this.draw.position = {x : 0, y : 0}
-        this.addChild(this.draw);
-        if(this.count < 2)
-            this.Hide()
-        else
-            this.Show();
-    }
-    
-    CountLabel.prototype.Show = function(){
-        this.visible = true;
-    }
-    
-    CountLabel.prototype.Hide = function(){
-        this.visible = false;
-    }
+define(['global', 'options', 'item', 'trackbar', 'count_label'], function(GLOBAL, OPTIONS, Item, trackbar, CountLabel){
     
     function find_slot(item){
         return undefined;
