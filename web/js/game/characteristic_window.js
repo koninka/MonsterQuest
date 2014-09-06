eefcbbe41e92fc8e53b29303e7b5fb14f0c3c32c
Mukhin Alexey <boogerm2@mail.ru>
2014-09-07 01:10:40 +1100

    add chr_wnd module
    shows player stats

---
+++ web/js/game/characteristic_window.js
@@ -0,0 +1,108 @@
+define(['global', 'options', 'count_label'], function(GLOBAL, OPTIONS, CountLabel){
+    var max_x = 0;
+    function CharacteristicWindow(char_list){
+        PIXI.DisplayObjectContainer.call(this);
+        this.background_layer      = new PIXI.DisplayObjectContainer();
+        this.characteristics_layer = new PIXI.DisplayObjectContainer();
+        this.counter_layer         = new PIXI.DisplayObjectContainer();
+        //this.add_buttons_layer     = new PIXI.DisplayObjectContainer();
+        this.addChild(this.background_layer);
+        this.addChild(this.characteristics_layer);
+        this.addChild(this.counter_layer);
+        //this.addChild(this.add_buttons_layer);
+        GLOBAL.graphic.stage.addChild(this);
+        this.characteristics_layer.characteristics = {};
+        this.free_points = 0;
+        this.InitBackground();
+        this.InitCharacteristics(char_list);
+        this.position = OPTIONS.chrwind.position;
+        this.Hide();
+    }
+
+    CharacteristicWindow.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+    CharacteristicWindow.prototype.constructor = CharacteristicWindow;
+
+    CharacteristicWindow.prototype.Show = function(){
+        this.visible = true;
+    }
+
+    CharacteristicWindow.prototype.Hide = function(){
+        this.visible = false;
+    }
+
+    CharacteristicWindow.prototype.Toggle = function(){
+        this.visible = !this.visible;
+    }
+
+    CharacteristicWindow.prototype.HideAddButtons = function(){
+        this.add_buttons_layer.visible = false;
+    }
+
+    CharacteristicWindow.prototype.ShowAddButtons = function(){
+        this.add_buttons_layer.visible = true;
+    }
+
+    CharacteristicWindow.prototype.AddToBackground = function(name, x, y){
+        return GLOBAL.graphic.AddSpriteToLayer(name, this.background_layer, x, y);
+    }
+
+    CharacteristicWindow.prototype.InitBackground = function() {
+        var top = this.AddToBackground('characteristics_window_top', 0, 0);
+        var middle = this.AddToBackground('characteristics_window_middle',  1, top.height);
+        var bottom = this.AddToBackground('characteristics_window_bottom',  0, top.height + middle.height);
+    };
+
+    //CharacteristicWindow.prototype.AddButton = function(x, y, characteristic_name){
+    //    var btn = GLOBAL.graphic.AddSpriteToLayer('add_characteristic_btn', this.add_buttons_layer, x, y);
+    //    btn.interactive = true;
+    //    btn.click = function(data){
+    //        data.originalEvent.preventDefault();
+    //        GLOBAL.game.sendViaWS({action: "add_characteristic", characteristic : characteristic_name});
+    //    }
+    //    return btn;
+    //}
+
+    function CorrectPosition(layer, x){
+        if (layer.position.x < x)
+            layer.position.x = x;
+    }
+
+    CharacteristicWindow.prototype.AddCharacteristic = function(name, count, x, y){
+        var t = this.characteristics_layer.characteristics[name] = GLOBAL.graphic.Text(
+            name + ' : ', 
+            {'font': '12px Helvetica', 'font-weight': 'bold', fill: 'white', wordWrap : true, wordWrapWidth : 200},
+            0, 
+            0
+        );
+        GLOBAL.graphic.AddObjToLayer(t, this.characteristics_layer, x, y);
+        x += t.width + OPTIONS.chrwind.offset;
+        t.counter = new CountLabel();
+        t.counter.SetCount(count);
+        t.counter.Show();
+        GLOBAL.graphic.AddObjToLayer(t.counter, this.counter_layer, 0, y);
+        CorrectPosition(this.counter_layer, x);
+        //x += t.counter.width + OPTIONS.chrwind.offset;
+        //CorrectPosition(this.add_buttons_layer, x);
+        return t;
+    }
+
+    CharacteristicWindow.prototype.InitCharacteristics = function(char_list){
+        var x = OPTIONS.chrwind.start.x;
+        var y = OPTIONS.chrwind.start.y;
+        for(var c in char_list){
+            y += OPTIONS.chrwind.offset;
+            this.AddCharacteristic(c, char_list[c], x, y);
+        }
+    }
+
+    CharacteristicWindow.prototype.UpdateCount = function(char_list){
+        for(var c in char_list){
+            var counter = this.characteristics_layer.characteristics[name].counter;
+            counter.SetCount(char_list[c]);
+            counter.Show();
+        }
+    }
+
+
+    return CharacteristicWindow;
+})
